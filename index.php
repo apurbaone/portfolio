aafbf59ae0381f9946082696a00b115ed2168d8f
apurbaone <one.apurba@gmail.com>
2025-04-26 22:17:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="description">
+  <meta content="" name="Apurba, system, admin, Dreameer, tech, enthuist">
+  <title>Apurba's Blog</title>
+  <link href="img/AB.png" rel="icon">
+  
+  
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link href="vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+  <header id="header">
+    <div class="d-flex flex-column">
+      <div class="profile">
+        <img src="img/PP400.png" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Apurba Barai</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://github.com/takenic" target="_blank"> <i class="bx bxl-github"> </i></a>
+          <a href="https://www.linkedin.com/in/apurba-barai-02191453/" target="_blank"> <i class="bx bxl-linkedin"></i></a>
+          <a href="https://instagram.com/baraiapurba" target="_blank"> <i class="bx bxl-instagram"> </i></a>
+          <!--<a href="https://linktr.ee/kikogoncalves/" target="_blank">
+            <span class="iconify mb-1" data-icon="simple-icons:linktree"></span> -->
+          </a>
+           <a href="mailto:one.apurba@gmail.com" target="_blank" class="email"> <i class="bx bx-envelope"> </i></a> 
+           <a href="https://www.upwork.com/freelancers/~01d781cc3b711ca7b3" target="_blank"> <i class="bx bx-link"></i></a>
+        </div>
+      </div>
+      <nav class="nav-menu">
+        <ul>
+          <li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about"> <i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#skills"> <i class="bx bx-file-blank"></i> <span>Skills</span></a></li>
+          <!--<li><a href="#resume"> <i class="bx bx-file-blank"></i> <span>Resume</span></a></li> -->
+          <li><a href="#portfolio"> <i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+          <li><a href="#contact"> <i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav>
+      <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+    </div>
+  </header>
+
+  <!----------- Main front Section ----------->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Apurba Barai</h1>
+      <p>I'm a <span class="typed"
+          data-typed-items="Tech Enthusiast, Dreamer, Traveller "></span></p>
+    </div>
+  </section>
+
+
+  <main id="main">
+    <!----------- About Section ----------->
+    <section id="about" class="about">
+      <div class="container">
+        <div class="section-title">
+          <h2>About</h2>
+          <p class="font-italic">
+            When technology meets creativity...
+          </p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="img/myimg.jpeg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Engineer Traveller Dreamer</h3>
+            <div class="row">
+              <div class="col-lg-8">
+                <ul id="facts">
+                  <li><i class="icofont-rounded-right"></i> <b>Age: </b> age</li>
+                  <li><i class="icofont-rounded-right"></i> <b>City:</b> Dhaka, Bangladesh</li>
+                  <li><i class="icofont-rounded-right"></i> <b>Birthday:</b>  Can't Disclose</li>
+                  <!-- <li><i class="icofont-rounded-right"></i> <b>Phone:</b> +351 91 827 11 05</li> -->
+                  <li><i class="icofont-rounded-right"></i> <b>Degree:</b> BSc in Computer Engineering</li>
+                  <li><i class="icofont-rounded-right"></i> <b>Occupation:</b> System Administrator
+                    
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <p>
+              I am an outdoor enthusiast and a cyclist. I use my “Fawn”, a model of Raily Bicycle for my daily commute and to travel the countryside. I am an experienced volunteer member in multiple travelling clubs. Since 2009 I have been participating in different expedition trips, rescue operations in crisis periods and different events here in Bangladesh.
+
+              <!-- <span class="text-muted">Always be grateful, hopeful and surround yourself of what you love.</span> -->
+            </p>
+            <ul>
+              <li>
+                <i class="icofont-info-square"></i>
+                <span>
+                  Check out my <b><a href="https://www.apurba.one/" target="_blank">Blog</a></b> to see
+                  more from me
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!----------- Skills Section ----------->
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+        <div class="section-title">
+          <h2>Skills</h2>
+          <h6>
+            Check out my <b><a href="https://github.com/takenic" target="_blank">github</a></b> page to find out
+            more about my portfolio
+          </h6>
+          <h6>
+            Below you can find a set of <b>Servers</b> <b>,Scripts</b> and <b>Technologies</b>
+            I have worked with assessed using a mildly accurate scale.
+          </h6>
+          <br>
+        </div>
+        <div class="row skills-content">
+          <div class="col-lg-6" data-aos="fade-up"> </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100"> </div>
+        </div>
+        <script src="js/skills.js" defer></script>
+    </section>
+
+ <!----------- portfolio Section ----------->
+ <section id="portfolio" class="skills section-bg">
+  <div class="container">
+    <div class="section-title">
+      <h2>portfolio</h2>
+      <div>
+        <table class="fixed">
+          <col width="300px" />
+          <col width="700px" />
+          <col width="300px" />
+          <thead>
+            <th class="table-header" colspan="3"> Live Projects </th>
+          </thead>
+          <tbody>
+            <tr>
+              <th class="tablerow-header">E-commerce</th>
+              <td style="width: 15.5cm;"> Single Website for E-commerce Busness
+              </td>
+              <td>
+                <a href="https://comfycloset.shop" target="_blank">Visit</a>
+                <br>
+                <a href="https://comfycloset.shop" target="_blank">The Website</a>
+              </td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">Restrurent</th>
+              <td>Restrurent with booking Table</td>
+              <td><a href="https://delareina.cafe" target="_blank">Website</a></td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">IT Firm</th>
+              <td>website for IT Busness<br>
+                
+              </td>
+              <td>
+                <a href="https://opelogy.com"
+                  target="_blank">Live Project Here</a>
+                <!--<br>
+                <a href="https://github.com/kiko-g/feup-lpoo/blob/master/project/docs/demo.gif"
+                  target="_blank">Project
+                  Demo</a> -->
+              </td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">Running Event Website</th>
+              <td>Website for an event<br>
+              <td><a href="https://marinedriveultra.run" target="_blank">Visit</a></td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">Linux</th>
+              <td>Virtualmin webserver for web-hosting Service</td>
+              <td><a href="https://vps1.apurba.one" target="_blank">See Here</a></td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">Urban Firm Project</th>
+              <td>On Going Project</td>
+              <td><a href="https://terracity.org" target="_blank">Visit website</a></td>
+            </tr>
+           <tr>
+              <th class="tablerow-header">Machine Code</th>
+              <td>Simple Machine Code for Water Level Masurement</td>
+              <td>
+                <a href="https://github.com/takenic/WLM" target="_blank">Git link Here</a>
+              </td>
+            </tr>
+            <!-- <tr>
+              <th class="tablerow-header">C programming w/ peripherals</th>
+              <td>Computer Labs modules (Timer, KBD, PS/2 mouse, etc.)<br>Space Lander game using those
+                modules</td>
+              <td><a href="https://github.com/kiko-g/feup-lcom#lcom-2019-2020-repository" target="_blank">LCOM</a>
+              </td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">C for networking</th>
+              <td>Computer Networking course projects (RCOM)
+                <ul>
+                  <li>Serial Port file transfer</li>
+                  <li>FTP download app and configuration and analysis of a network</li>
+                </ul>
+              </td>
+              <td>
+                <a href="https://github.com/kiko-g/feup-rcom/tree/master/P1#rcom-20192020" target="_blank">Serial
+                  Port</a>
+                <br>
+                <a href="https://github.com/kiko-g/feup-rcom/tree/master/P2" target="_blank">FTP Download
+                  App</a>
+              </td>
+            </tr>
+            <tr>
+              <th class="tablerow-header">Python and Jupyter NB</th>
+              <td>AI course projects (IART)
+                <ul>
+                  <li>Optimization problem: Self Driving Rides (Google Hashcode)</li>
+                  <li>Supervised learning models for Football Outcome Predictions</li>
+                </ul>
+              </td>
+              <td>
+                <a href="https://github.com/kiko-g/feup-iart/tree/master/Projects/P1#self-driving-rides"
+                  target="_blank">Google Hashcode 2018</a>
+                <br>
+                <a href="https://github.com/kiko-g/feup-iart/tree/master/Projects/P2#supervised-learning-models-used-to-predict-football-matches-outcomes"
+                  target="_blank">Football outcome predictions</a>
+              </td>
+            </tr>-->
+          </tbody>
+        </table>
+      </div>
+    </div>
+      
+    
+</section>
+
+<!----------- contact Section ----------->
+<section id="contact" class="skills section-bg">
+  <div class="container">
+    <div class="section-title">
+      <h2>Contact</h2>
+      <div class="row" data-aos="fade-in">
+        <div class="col-lg-5 d-flex align-items-stretch">
+          <div class="info">
+
+            <div class="phone">
+              <i class="icofont-phone"></i>
+              <a href="https://github.com/takenic" target="_blank"> <i class="bx bxl-github"> </i></a>
+          
+              <h4>Call</h4>
+              <p>+88 01534906092</p>
+            </div>
+<br>
+            <div class="email">
+              <i class="bx bx-envelope"></i>
+              <h4>Personal Email</h4>
+              <p><a href="mailto:one.apurba@gmail.com">one.apurba@gmail.com</a></p>
+            </div>
+<br>
+            <div class="email">
+              <i class="icofont-email"></i>
+              <h4>WEB Email</h4>
+              <p><a href="mailto:mail@apurba.one">mail@apurba.one</a></p>
+            </div>
+<br>
+            <div class="address">
+              <i class="icofont-google-map"></i>
+              <h4>Location</h4>
+              <p>Dhaka, Bangladesh</p>
+            </div>
+
+            <iframe width="101%" height="300"
+              
+              frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+          </div>
+        </div>
+
+        <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+          <form action="assets/forms/contact.php" method="post" role="form" class="php-email-form">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="name">Your Name</label>
+                <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4"
+                  data-msg="Please enter at least 4 chars" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <label for="name">Your Email</label>
+                <input type="email" class="form-control" name="email" id="email" data-rule="email"
+                  data-msg="Please enter a valid email" />
+                <div class="validate"></div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="name">Subject</label>
+              <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4"
+                data-msg="Please enter at least 8 chars of subject" />
+              <div class="validate"></div>
+            </div>
+            <div class="form-group">
+              <label for="name">Message</label>
+              <textarea class="form-control" name="message" rows="10" data-rule="required"
+                data-msg="Please write something for us"></textarea>
+              <div class="validate"></div>
+            </div>
+            
+            <div class="text-center"><button type="submit">Send Message</button></div>
+          </form>
+        </div>
+
+      </div>
+      
+</section>
+
+</main>
+
+
+  <script>
+    let age = new Date(Date.now() - new Date(1992, 15, 6).getTime()).getUTCFullYear() - 1970;
+    document.getElementById("facts").childNodes[1].lastChild.data = age;
+    
+  </script> 
+
+  <!-- --------- Footer --------- -->
+  <footer id="footer">
+    <div class="container">
+      <div class="credits">
+        <span class="text-muted">Developed by <a href="https://apurba.one/">Apurba</a></span>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+  <script src="vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="vendor/counterup/counterup.min.js"></script>
+  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="vendor/venobox/venobox.min.js"></script>
+  <script src="vendor/typed.js/typed.min.js"></script>
+  <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="vendor/aos/aos.js"></script>
+
+  <!--  -->
+  <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+  <!-- <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script> -->
+
+  <!-- Main JS File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
